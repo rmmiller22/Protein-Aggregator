88b3b6c7f8924fd275a36311368ded9081a7ffc5
Rachel Miller <rmmiller22@wisc.edu>
2017-08-04 16:29:35 -0500

    Add project files.

---
+++ ProteinAggregator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsefulProteomicsDatabases;
+using Proteomics;
+using System.Collections;
+using System.Xml;
+
+
+namespace ProteinAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader(@"E:\RMM\Jurkat Pruned Multiprotease databases\Trypsin\uniprot-proteome%3AUP000005640.xml-Jurkat_SAV_DBGPTMDproteinPruned.xml");
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine("<");
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+        }
+    }
+}
